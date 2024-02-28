27a3f21889f3c4c7a7f22f56d322d4fac1634ebc
Jhon Sebastian Bohorquez Guerrero <js.bohorquezg1@uniandes.edu.co>
2024-02-28 04:05:21 -0500

    prueba test

--- posts/tests/blueprints/test_posts.py
+++ posts/tests/blueprints/test_posts.py
@@ -110,20 +110,20 @@ class TestPosts():
   #       assert 'plannedEndDate' in response_json
   #       assert 'createdAt' in response_json
   
-  # def test_get_post_without_token(self):
-  #   data = {
-  #     'routeId': '1',
-  #     'expireAt': "2024-06-17T02:21:49.025Z"
-  #   }
-  #   userId = '09322959-5bd7-4fdb-b262-ab46dab67c68'
-  #   post = CreatePost(data, userId).execute()
+  def test_get_post_without_token(self):
+    data = {
+      'routeId': '1',
+      'expireAt': "2024-06-17T02:21:49.025Z"
+    }
+    userId = '09322959-5bd7-4fdb-b262-ab46dab67c68'
+    post = CreatePost(data, userId).execute()
 
-  #   with app.test_client() as test_client:
-  #     with HTTMock(mock_failed_auth_outtoken):
-  #       response = test_client.get(
-  #         f'/posts/{post["id"]}'
-  #       )
-  #       assert response.status_code == 403
+    with app.test_client() as test_client:
+      with HTTMock(mock_failed_auth_outtoken):
+        response = test_client.get(
+          f'/posts/{post["id"]}'
+        )
+        assert response.status_code == 403
 
   # def test_get_post_invalid_token(self):
   #   data = {
