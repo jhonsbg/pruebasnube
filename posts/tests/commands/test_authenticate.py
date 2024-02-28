e5d4079de58616ff381d8ff40b73f09422167cb3
Jhon Sebastian Bohorquez Guerrero <js.bohorquezg1@uniandes.edu.co>
2024-02-28 03:46:14 -0500

    prueba test

--- posts/tests/commands/test_authenticate.py
+++ posts/tests/commands/test_authenticate.py
@@ -9,8 +9,7 @@ from tests.mocks import mock_failed_auth, mock_success_auth
 class TestAuthenticate():
   def test_authenticate(self):
     with HTTMock(mock_success_auth):
-      token = str(uuid4())
-      result = Authenticate(token).execute()
+      result = Authenticate('Bearer 09322959-5bd7-4fdb-b262-ab46dab67c68').execute()
       assert 'id' in result
 
   def test_failed_authenticate(self):
